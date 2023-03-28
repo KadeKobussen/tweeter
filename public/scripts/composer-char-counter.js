8d0e72d5e9586cf1ac5407f49d68f0a99c84a585
Kade Kobussen <kadekobussen50@gmail.com>
2023-03-28 15:19:43 -0700

    add html and css for posted tweets.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  console.log('sdssfsgsrf')
+  $('.new-tweet form textarea').on('input', function() {
+    let tweetLength = $(this).val().length;
+    let remainingChars = 140 - tweetLength;
+    let counter = $(this).siblings('.row').find('.counter');
+    counter.text(remainingChars);
+    if (remainingChars < 0) {
+      counter.css('color', 'red');
+    } else {
+      counter.css('color', '#244751');
+    }
+  });
+});

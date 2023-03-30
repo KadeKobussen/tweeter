5af978f7db8379a6a668cd9f9b5724dc97244846
Kade Kobussen <kadekobussen50@gmail.com>
2023-03-30 16:28:19 -0700

    organise new-tweet.css in a logical order.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,10 @@
 
-
 $(document).ready(function() {
   // Counter for remaining characters in tweet
   $('.new-tweet form textarea').on('input', function() {
     let tweetLength = $(this).val().length;
     let remainingChars = 140 - tweetLength;
-    let counter = $(this).siblings('.row').find('.counter');
+    let counter = $(this).siblings('.button-counter').children('.counter');
     counter.text(remainingChars);
     if (remainingChars < 0) {
       counter.css('color', 'red');
